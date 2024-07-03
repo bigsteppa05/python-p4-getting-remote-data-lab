094c9a6c9b224d3de62e29e32dd4526829429273
bigsteppa05 <habibforderiv05@gmail.com>
2024-07-03 14:37:39 +0300

    getting remote lab

--- lib/testing/get_requester_test.py
+++ lib/testing/get_requester_test.py
@@ -1,16 +1,15 @@
-from GetRequester import GetRequester
-class GetRequesterTest:
-    '''Class {Classname} in {modulename}.py'''
-URL = 'https://learn-co-curriculum.github.io/json-site-example/endpoints/people.json'
-JSON_STRING = b"[\n  {\n    \"name\": \"Daniel\",\n    \"occupation\": \"LG Fridge Salesman\"\n  },\n  {\n    \"name\": \"Joe\",\n    \"occupation\": \"WiFi Fixer\"\n  },\n  {\n    \"name\": \"Avi\",\n    \"occupation\": \"DJ\"\n  },\n  {\n    \"name\": \"Howard\",\n    \"occupation\": \"Mountain Legend\"\n  }\n]\n"
-CONVERTED_DATA = [{ 'name': 'Daniel', 'occupation' : 'LG Fridge Salesman' }, { 'name': 'Joe', 'occupation': 'WiFi Fixer' }, { 'name': 'Avi', 'occupation': 'DJ' }, { 'name': 'Howard', 'occupation': 'Mountain Legend' }]
+import requests
+import json
 
-def test_get_response():
-        '''get_response_body function returns response.'''
-        requester = GetRequester(URL)
-        assert(requester.get_response_body() == JSON_STRING)
+class GetRequester:
 
-def test_load_json():
-        '''load_json function returns response.'''
-        requester = GetRequester(URL)
-        assert(requester.load_json() == CONVERTED_DATA)
+    def __init__(self, url):
+        self.url = url
+
+    def get_response_body(self):
+
+        response = requests.get(self.url)
+        return response.content
+
+    def load_json(self):
+        return json.loads(self.get_response_body())
